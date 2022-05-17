d4b999cf4194f663115181a7c69d3d766d993927
전지혜 <jeonjh9535@naver.com>
2022-05-17 16:20:52 +0900

    설명문구만 우선 실 데이터 반영

--- app/src/main/java/com/asianaidt/jicbangcopy_20220517/MainActivity.kt
+++ app/src/main/java/com/asianaidt/jicbangcopy_20220517/MainActivity.kt
@@ -2,10 +2,31 @@ package com.asianaidt.jicbangcopy_20220517
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.asianaidt.jicbangcopy_20220517.adapters.RoomAdapter
+import com.asianaidt.jicbangcopy_20220517.datas.RoomData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val mRoomList = ArrayList<RoomData>()
+    lateinit var mRoomAdaper : RoomAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mRoomList.add(RoomData(8000,"서울시 강동구",1,"1번째 방입니다"))
+        mRoomList.add(RoomData(11000,"서울시 서대문구",10,"2번째 방입니다"))
+        mRoomList.add(RoomData(12000,"서울시 동작구",15,"3번째 방입니다"))
+        mRoomList.add(RoomData(5000,"서울시 종로구",-2,"4번째 방입니다"))
+        mRoomList.add(RoomData(70000,"서울시 강서구",21,"5번째 방입니다"))
+        mRoomList.add(RoomData(22000,"서울시 송파구",17,"6번째 방입니다"))
+        mRoomList.add(RoomData(14000,"서울시 은평구",4,"7번째 방입니다"))
+        mRoomList.add(RoomData(22000,"서울시 성동구",2,"8번째 방입니다"))
+        mRoomList.add(RoomData(6670,"서울시 구로구",-2,"9번째 방입니다"))
+
+        mRoomAdaper = RoomAdapter(this,R.layout.room_list_item,mRoomList)
+        roomListView.adapter = mRoomAdaper
+
     }
 }
--- app/src/main/java/com/asianaidt/jicbangcopy_20220517/adapters/RoomAdapter.kt
+++ app/src/main/java/com/asianaidt/jicbangcopy_20220517/adapters/RoomAdapter.kt
@@ -1,9 +1,12 @@
 package com.asianaidt.jicbangcopy_20220517.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.asianaidt.jicbangcopy_20220517.R
 import com.asianaidt.jicbangcopy_20220517.datas.RoomData
 
 class RoomAdapter(  val mContext: Context,
@@ -11,7 +14,26 @@ class RoomAdapter(  val mContext: Context,
                     val mList : ArrayList<RoomData>) : ArrayAdapter<RoomData>(mContext,resId,mList) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        return super.getView(position, convertView, parent)
+
+        var tempRow = convertView
+
+        if(tempRow == null){
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.room_list_item,null)
+
+        }
+        val row = tempRow!!
+
+        val data = mList[position]
+
+        val txtPrice = row.findViewById<TextView>(R.id.txtPrice)
+        val txtAddress = row.findViewById<TextView>(R.id.txtAddress)
+        val txtFloor = row.findViewById<TextView>(R.id.txtFloor)
+        val txtDesc = row.findViewById<TextView>(R.id.txtDesc)
+
+        txtDesc.text = data.desc
+
+        return row
+
     }
 
 }
